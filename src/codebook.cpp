3f2085b68401f02af66d5733f654563fe6120462
tinray <b03902072@ntu.edu.tw>
2016-12-08 16:00:42 +0800

    make some args positional

--- src/codebook.cpp
+++ src/codebook.cpp
@@ -14,13 +14,15 @@ Arguments getArgs(int argc, char**argv) {
     Arguments args;
     try {
         namespace po = boost::program_options;
-        po::options_description required("Required arguments:");
-        required.add_options()
-            ("source", po::value<std::string>(&args.source) -> required(), "source.txt")
-            ("target", po::value<std::string>(&args.target) -> required(), "test.txt");
 
-        po::options_description optional("Optional arguments:");
-        optional.add_options()
+        po::positional_options_description positional;
+        positional.add("source", 1);
+        positional.add("target", 1);
+
+        po::options_description desc("===== Codebook =====");
+        desc.add_options()
+            ("source", po::value<std::string>(&args.source) -> required(), "source.txt")
+            ("target", po::value<std::string>(&args.target) -> required(), "test.txt")
             ("help", "Print help message.")
             ("nmtfRate", po::value<double>(&args.nmtfRate) -> default_value(0.0001), "learning rate for nmtf")
             ("converge", po::value<double>(&args.nmtfConv) -> default_value(10), "converge criteria for nmtf")
@@ -29,10 +31,11 @@ Arguments getArgs(int argc, char**argv) {
             ("nClustersU", po::value<int>(&args.nClustersU) -> default_value(10), "number of user clusters")
             ("nClustersI", po::value<int>(&args.nClustersI) -> default_value(10), "number of item clusters");
 
-        po::options_description desc("===== Codebook =====");
-        desc.add(required).add(optional);
         po::variables_map vm;
-        po::store(po::parse_command_line(argc, argv, desc), vm);
+        po::store(po::command_line_parser(argc, argv)
+                  .options(desc)
+                  .positional(positional)
+                  .run(), vm);
 
         if (vm.count("help")) {
             std::cout << desc << std::endl;
@@ -55,7 +58,12 @@ int main(int argc, char**argv){
     Eigen::MatrixXd sourceMask(args.rD1, args.rD2);
     loadMatrix(args.source, sourceRate, sourceMask);
 
-    nmtf(args.nmtfRate, args.nmtfConv, sourceRate, sourceMask);
+    Eigen::MatrixXd s;
+    Eigen::MatrixXd f;
+    Eigen::MatrixXd g;
+    nmtf(args.nmtfRate, args.nmtfConv,
+         args.nClustersU, args.nClustersI,
+         sourceRate, sourceMask, f, s, g);
     
     return 0;
 }
