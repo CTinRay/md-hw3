f2e75aa6f600dd29dec03ba9f0d7efed0d402f14
CTinRay <b03902072@ntu.edu.tw>
2016-12-10 21:07:37 +0800

    add utils to load cluster result and model

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,8 +1,10 @@
 #include "utils.hpp"
 #include <fstream>
 #include <cassert>
+#include <iostream>
+#include <cassert>
 
-void loadMatrix(const std::string filename,
+void loadMatrix(const std::string& filename,
                 Eigen::MatrixXd&matrix, Eigen::MatrixXd&mask){
     assert(matrix.rows() == mask.rows());
     assert(matrix.cols() == mask.cols());
@@ -18,5 +20,52 @@ void loadMatrix(const std::string filename,
         matrix(x, y) = rate;
         mask(x, y) = 1;
         f >> x >> y >> rate;   
+    }
+}
+
+
+void loadModel(const std::string& filename,
+               Eigen::MatrixXd& m1, Eigen::MatrixXd& m2) {
+    std::fstream f;
+    f.open(filename, std::ios::in);
+    int f0, m, n, k;
+    float b;
+    std::string buf;
+    f >> buf >> f0;
+    f >> buf >> m;
+    f >> buf >> n;
+    f >> buf >> k;
+    f >> buf >> b;
+
+    m1 = Eigen::MatrixXd::Zero(m, k);
+    m2 = Eigen::MatrixXd::Zero(n, k);
+    
+    for (auto i = 0; i < m; ++i) {
+        f >> buf;
+        f >> buf;
+        for (auto j = 0; j < k; ++j) {
+            f >> m1(i, j);
+        }
+    }
+
+    for (auto i = 0; i < n; ++i) {
+        f >> buf >> buf;
+        for (auto j = 0; j < k; ++j) {
+            f >> m2(i, j);
+        }
+    }
+
+    f.close();
+}
+
+
+void loadMem(const std::string& filename, Eigen::MatrixXd& mem) {
+    std::fstream f;
+    f.open(filename, std::ios::in);
+    for (auto i = 0u; i < mem.rows(); ++i) {
+        int j = 0;
+        f >> j;
+        assert(j >= 0 && j < mem.cols());
+        mem(i, j) = 1;
     }
 }
