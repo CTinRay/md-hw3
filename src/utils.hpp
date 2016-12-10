d68befbecaa8030a9ef15cacabd374470a288bf0
CTinRay <b03902072@ntu.edu.tw>
2016-12-10 21:10:02 +0800

    utils.hpp

--- src/utils.hpp
+++ src/utils.hpp
@@ -3,8 +3,12 @@
 #ifndef UTILS_HPP
 #define UTILS_HPP
 
-void loadMatrix(const std::string filename,
+void loadMatrix(const std::string& filename,
                 Eigen::MatrixXd&matrix, Eigen::MatrixXd&mask);
 
+void loadModel(const std::string& filename,
+               Eigen::MatrixXd& m1, Eigen::MatrixXd& m2);
+
+void loadMem(const std::string& filename, Eigen::MatrixXd& mem);   
 
 #endif
